e27cf2d8a113fb760498c341c0022236dddf9f44
jhonas <jhonas.burlamaqui@gmail.com>
2024-05-17 02:00:27 +0100

    new

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,48 @@
+local opts = { noremap = true, silent = true }
+vim.g.mapleader = " "
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<tab>", "<C-w>w", opts)
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "n", "nzz", opts)
+vim.keymap.set("n", "N", "Nzz", opts)
+vim.keymap.set("n", "*", "*zz", opts)
+vim.keymap.set("n", "#", "#zz", opts)
+vim.keymap.set("n", "g*", "g*zz", opts)
+vim.keymap.set("n", "g#", "g#zz", opts)
+
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set("v", "p", '"_dP', opts)
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+
+-- what we yank is what is in the register after we past something on top of something
+vim.keymap.set("x", "p", [["_dP]])
+
+vim.keymap.set("n", "<leader>w", ":w<Cr>", opts)
+vim.keymap.set("n", "<leader>q", ":q!<Cr>", opts)
+
+vim.keymap.set("n", "<leader>nh", ":nohlsearch<Cr>", opts)
+
+vim.keymap.set("i", "jj", "<Esc>", opts)
