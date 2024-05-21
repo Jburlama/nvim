b236506a4da917c5159b2c3e885cadec3c2bbd3d
Jburlama <jhonas.burlamaqui@gmail.com>
2024-05-21 16:09:39 +0100

    <C-n> to use normal mode on the terminal

--- lua/plugins/floating_terminal.lua
+++ lua/plugins/floating_terminal.lua
@@ -5,7 +5,7 @@ return {
 		vim.keymap.set("n", "<C-n>", ":FloatermNew --height=1.0 --width=1.0 <Cr>", {})
 		vim.keymap.set("n", "<C-t>", ":FloatermToggle<Cr>", {})
 		vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:FloatermToggle<Cr>", {})
-		vim.keymap.set("t", "jj", "<C-\\><C-n>", {})
+		vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", {})
 
 	end
 }
