3238bb6e3eddd59d13e653fb59e98bf20758efc2
jhonas <jhonas.burlamaqui@gmail.com>
2024-07-02 19:45:43 +0100

    harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,14 +1,21 @@
 return {
-	"nvim-lua/plenary.nvim",
 	"ThePrimeagen/harpoon",
-
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	config = function ()
-
-		vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
-		vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
-		vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
-		vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
-		vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
-
+		require("harpoon").setup({})
+		vim.keymap.set("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<Cr>", {})
+		vim.keymap.set("n", "<leader>m",":lua require('harpoon.mark').add_file()<Cr>", {})
+		vim.keymap.set("n", "1",":lua require('harpoon.ui').nav_file(1) <Cr>", {})
+		vim.keymap.set("n", "2",":lua require('harpoon.ui').nav_file(2) <Cr>", {})
+		vim.keymap.set("n", "3",":lua require('harpoon.ui').nav_file(3) <Cr>", {})
+		vim.keymap.set("n", "4",":lua require('harpoon.ui').nav_file(4) <Cr>", {})
+		vim.keymap.set("n", "5",":lua require('harpoon.ui').nav_file(5) <Cr>", {})
+		vim.keymap.set("n", "6",":lua require('harpoon.ui').nav_file(6) <Cr>", {})
+		vim.keymap.set("n", "7",":lua require('harpoon.ui').nav_file(7) <Cr>", {})
+		vim.keymap.set("n", "8",":lua require('harpoon.ui').nav_file(8) <Cr>", {})
+		vim.keymap.set("n", "<C-n>",":lua require('harpoon.ui').nav_next()  <Cr>", {})
+		vim.keymap.set("n", "<C-o>",":lua require('harpoon.ui').nav_prev()  <Cr>", {})
 	end
 }
