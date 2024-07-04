c456b1f1a8546664f5983cfa955143c513e7a04e
Jburlama <jhonas.burlamaqui@gmail.com>
2024-07-04 18:17:18 +0100

    harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,9 +8,9 @@ return {
 		vim.keymap.set("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<Cr>", {})
 		vim.keymap.set("n", "<leader>mh",":lua require('harpoon.mark').add_file()<Cr>", {})
 		vim.keymap.set("n", "b",":lua require('harpoon.ui').nav_file(1) <Cr>", {})
-		vim.keymap.set("n", "d",":lua require('harpoon.ui').nav_file(2) <Cr>", {})
-		vim.keymap.set("n", "e",":lua require('harpoon.ui').nav_file(3) <Cr>", {})
-		vim.keymap.set("n", "m",":lua require('harpoon.ui').nav_file(4) <Cr>", {})
+		vim.keymap.set("n", "e",":lua require('harpoon.ui').nav_file(2) <Cr>", {})
+		vim.keymap.set("n", "m",":lua require('harpoon.ui').nav_file(3) <Cr>", {})
+		vim.keymap.set("n", "r",":lua require('harpoon.ui').nav_file(4) <Cr>", {})
 		vim.keymap.set("n", "<C-m>",":lua require('harpoon.ui').nav_next()  <Cr>", {})
 		vim.keymap.set("n", "<C-o>",":lua require('harpoon.ui').nav_prev()  <Cr>", {})
 	end
