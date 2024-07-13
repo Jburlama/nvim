5325625b9ddf9ad374b9da00844d9412c286f3c4
Jburlama <jhonas.burlamaqui@gmail.com>
2024-07-13 16:02:25 +0100

    enter normal mode inside the terminal with Esc

--- lua/plugins/floating_terminal.lua
+++ lua/plugins/floating_terminal.lua
@@ -5,7 +5,8 @@ return {
 		vim.keymap.set("n", "<C-n>", ":FloatermNew --height=1.0 --width=1.0 <Cr>", {})
 		vim.keymap.set("n", "<C-t>", ":FloatermToggle<Cr>", {})
 		vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:FloatermToggle<Cr>", {})
-		vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", {})
+		-- vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", {})
+		vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
 
 	end
 }
